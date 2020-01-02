bd820808b4daf73be74ca0833b8a886cedeb9cfc
David Slusser <dslusser@box.com>
2020-01-01 21:54:15 -0800

    fixed bug in recents/favorites view where base template wasn't correctly referenced.

--- userextensions/__init__.py
+++ userextensions/__init__.py
@@ -7,8 +7,9 @@ A user extension module for django.
 """
 
 __title__ = 'django-userextensions'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __author__ = 'David Slusser'
+__email__ = 'dbslusser@gmail.com'
 __license__ = 'GPL-3.0'
 __copyright__ = 'Copyright 2019 David Slusser'
 
